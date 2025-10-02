2bf65d49031b98bc68c418bfe53602482a7b5b99
Arthur Rocha <137656139+arthurRocha01@users.noreply.github.com>
2025-10-02 14:13:31 -0300

    Update script.js

--- exerc-js/script.js
+++ exerc-js/script.js
@@ -1,26 +1,46 @@
 // Exercício 1
 function showMessage() {
-    const messageField = document.getElementsByClassName('message-field')[0]
-    const input = document.getElementById('input').value
-    console.log(input)
-    messageField.innerHTML = input
+    const input = document.getElementById('showMessage-input')
+    const messageField = document.getElementById('showMessage-fieldDisplay')
+
+    messageField.textContent = input.value
 }
 
 // Exercício 2
 function sum() {
-    const number1 = Number(document.getElementById('number1').value)
-    const number2 = Number(document.getElementById('number2').value)
-    const sum = number1 + number2;
+    const resultField = document.getElementById('sum-fieldDisplay')
 
-    const resultField = document.getElementById('result-field')
+    const number1 = Number(document.getElementById('sum-number1').value)
+    const number2 = Number(document.getElementById('sum-number2').value)
+    const sum = number1 + number2;
     resultField.innerHTML = sum.toString()
 }
 
 // Exercício 3
 function changeColor(element) {
-    const field = document.getElementById('text-change-color')
+    const field = document.getElementById('changeColor-text')
 
     const color = element.id
-    console.log(color)
     field.style.color = color.toLowerCase()
+}
+
+// Exercício 4
+function addList() {
+    const input = document.getElementById('addList-input')
+    const listField = document.getElementById('addList-fieldDisplay')
+
+    const textInput = input.value
+    const newItem = document.createElement('li')
+    newItem.textContent = textInput
+    listField.append(newItem)
+}
+
+// Exercício 5
+function parityNumber() {
+    const input = document.getElementById('parity-numbers-input')
+    const displayField = document.getElementById('parity-numbers-field')
+
+    const number = input.value
+    if (number % 2 === 0) displayField.textContent = "O número é PAR"
+    else displayField.textContent = "O número é IMPAR"
 }
